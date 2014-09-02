fd30f42924b3a04a4ef6611d9de3b1d0afb25de5
Kostas Polakis <copolla87@gmail.com>
2014-09-02 13:43:18 +0300

    fixed indexes carousel now images fit the 1200x480.made changes in css .caroussel - inner and in index.php img src

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,5 +1,5 @@
   <div class="container">
-    <div id="this-carousel-id" class="carousel slide"><!-- class of slide for animation -->
+    <div id="this-carousel-id" class="carousel slide" ><!-- class of slide for animation -->
   <div class="carousel-inner">
     <div class="item active"><!-- class of active since it's the first item -->
       <img src="http://placehold.it/1200x480" alt="" />
@@ -10,34 +10,9 @@
 
     <?php foreach ($articles as $article) { ?>
     <div class="item">
-
-      
       <img src="<?php
-        /*
-        $img_src = $article->photo;
-        $imgbinary = fread(fopen($img_src, "r"), filesize($img_src));
-        $img_str = base64_encode($imgbinary);
-        echo '<img src="data:image/jpeg;base64,'.$img_str.'" />';
-        */
-
-        /*
-        $data = $article->photo;
-        $data = base64_decode($data);
-        $im = imagecreatefromstring($data);
-        header('Content-Type: image/jpg');
-        echo imagejpeg($im);
-        */
         echo $article->photo
-       /*
-        header("Content-type: image/jpeg");
-        $data = $article->photo;
-        $data = pack('H*',$data); 
-        $im = imagecreatefromstring($data);
-        echo imagejpeg($im);
-        */
-       
-        //echo $article->photo 
-       ?>"/>
+       ?>" style="height:480px; width:1200px"/>
       <div class="carousel-caption">
         <p><?php echo $article->title;?></p>
       </div>
@@ -50,3 +25,10 @@
     <a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
 </div><!-- /.carousel -->
   </div>  
+  <div class="container">
+    <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<p><br>
+    <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<p><br>
+    <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<p><br>
+    <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<p><br>
+    <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<p><br>
+  </div>
